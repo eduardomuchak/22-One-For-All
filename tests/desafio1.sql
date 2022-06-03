306701e2be8d2af26ee18293b41f62a9c208d37e
Eduardo Muchak <eduardomuchak@gmail.com>
2022-06-03 11:48:32 -0300

    feat: Requisito 1: -- Finaliza criação das tabelas do banco de dados

--- tests/desafio1.sql
+++ tests/desafio1.sql
@@ -19,10 +19,33 @@ CREATE TABLE SpotifyClone.artistas(
   nome_artista VARCHAR(255) NOT NULL,
 ) engine = InnoDB;
 
-CREATE TABLE SpotifyClone.seguindo_artistas(
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE SpotifyClone.artistas_usuarios(
+  id INT NOT NULL,
   FOREIGN KEY (`artista_id`) REFERENCES SpotifyClone.artistas(artista_id),
-  FOREIGN KEY (`usuario_id`) REFERENCES SpotifyClone.usuarios(usuario_id)
+  FOREIGN KEY (`usuario_id`) REFERENCES SpotifyClone.usuarios(usuario_id),
+  PRIMARY KEY (`artista_id`, `usuario_id`),
+) engine = InnoDB;
+
+CREATE TABLE SpotifyClone.albuns(
+  album_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  nome_album VARCHAR(255) NOT NULL,
+  FOREIGN KEY (`artista_id`) REFERENCES SpotifyClone.artistas(artista_id),
+  ano_lancamento INT NOT NULL,
+) engine = InnoDB;
+
+CREATE TABLE SpotifyClone.cancoes(
+  cancao_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  nome_cancao VARCHAR(255) NOT NULL,
+  FOREIGN KEY (`album_id`) REFERENCES SpotifyClone.albuns(album_id),
+  duracao_segundos INT NOT NULL,
+) engine = InnoDB;
+
+CREATE TABLE SpotifyClone.usuarios_cancoes(
+  id INT NOT NULL,
+  FOREIGN KEY (`usuario_id`) REFERENCES SpotifyClone.usuarios(usuario_id),
+  FOREIGN KEY (`cancao_id`) REFERENCES SpotifyClone.cancoes(cancao_id),
+  data_reproducao DATETIME NOT NULL,
+  PRIMARY KEY (`usuario_id`, `cancao_id`),
 ) engine = InnoDB;
 
 INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)

bd3fe2656376d5ef4d6656b3d7cbd551c2b49d56
Eduardo Muchak <eduardomuchak@gmail.com>
2022-06-06 08:56:29 -0300

    feat: Requisito 5 finalizado!

---
+++ desafio5.sql
@@ -0,0 +1,11 @@
+SELECT 
+	c.nome_cancao AS cancao, 
+	COUNT(uc.cancao_id) AS reproducoes
+FROM 
+	SpotifyClone.cancoes AS c
+    INNER JOIN
+	SpotifyClone.usuarios_cancoes AS uc
+    ON c.cancao_id = uc.cancao_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao ASC
+LIMIT 2;
